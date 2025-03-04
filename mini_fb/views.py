b561823a74982f30c855efb96228f3f8092632a2
Elitsa Mincheva <e.mincheva@gmail.com>
2025-03-04 11:39:21 -0500

    add image to status message

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -2,12 +2,12 @@
 # - ShowAllProfilesView: Displays a list of all profiles
 # - ProfileView: Displays a single profile based on the profile's primary key (pk)
 # - CreateProfileView: create a new profile in the FB
-# - CreateStatusMessageView: create a new status message for a Profile
+# - CreateStatusMessageView: create a new status message for a Profile, can add photos to go along with the status message
 
 from django.urls import reverse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
-from .models import Profile, StatusMessage
+from .models import Profile, StatusMessage, Image, StatusImage
 from .forms import CreateProfileForm, CreateStatusMessageForm
 
 # Create your views here.
@@ -45,16 +45,34 @@ class CreateStatusMessageView(CreateView):
         context['profile'] = profile
         return context
 
+
     def form_valid(self, form):
-        '''handle the form submission and save the new object to the database. add foregin key (of the profile)
-        to the StatusMessage object before saving it to the database'''
-        print(f"CreateStatusMessageView.form_valid: form.cleaned_data={form.cleaned_data}")
-        # retrieve the PK from the URL pattern
+        """Handle the form submission, save the new status message, and associate uploaded images."""
+        # print(f"CreateStatusMessageView.form_valid: form.cleaned_data={form.cleaned_data}")
+        
+        # retrieve the profile based on the PK from the URL
         pk = self.kwargs['pk']
         profile = Profile.objects.get(pk=pk)
-        # attach this profile to the status message
+        
+        # attach this profile to the status message and save it
         form.instance.profile = profile  
+        sm = form.save()  # save status message and get the reference
+
+        # Read uploaded files
+        files = self.request.FILES.getlist('files')  
+        # print(f"Uploaded files: {files}")  
+
+        # process each uploaded file
+        for file in files:
+            # create and save an image object
+            img = Image(profile=profile, image_file=file)
+            img.save()
+            # create and save a StatusImage object linking the image and status message
+            status_img = StatusImage(status_message=sm, image=img)
+            status_img.save()
+            # print(f"Saved image {img.id} and linked it to status message {sm.id}")
         return super().form_valid(form)
+
         
     def get_success_url(self):
         '''provide a URL to redirect to after creating a new StatusMessage'''
